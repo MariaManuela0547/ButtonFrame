724775242ac047e7026ca0f70d1d88805282149e
MariaManuela0547 <124294920+MariaManuela0547@users.noreply.github.com>
2023-03-09 15:18:17 -0300

    Add files via upload

---
+++ ButtonTest.java
@@ -0,0 +1,15 @@
+
+package javaapplication3;
+
+import javax.swing.JFrame;
+
+public class ButtonTest 
+{
+    public static void main(String[] args)
+    {
+        JavaApplication3 buttonFrame = new JavaApplication3();
+        buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonFrame.setSize(275,110);
+        buttonFrame.setVisible(true);
+    }
+}
